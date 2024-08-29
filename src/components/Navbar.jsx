a98f63f4057f2cd301099066402388eab477a6b0
caitlinanderson099 <caitlin.anderson099@gmail.com>
2024-08-29 22:27:28 +1200

    added the portfolio logo to browser tab and changed browser tab name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,8 @@ import { useState } from 'react'
 const Navbar = () => {
   return (
     <div id='top-nav'>
-        <div id='logo'>
-            {/* img here */}
-        </div>
         <div className='nav-items'>
-            <Link to='/'> <h4> Home </h4> </Link> |
+            <Link to='/'> <h4> Home </h4> </Link> 
             <Link to='/projects'> <h4> Projects </h4> </Link>
         </div>
         
